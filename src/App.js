28516a8ece6575a5783d91299a2d8ca816d3cc7b
Shahid ul Islam <shahidsalafi4@gmail.com>
2023-05-16 10:37:01 +0530

    First Commit, file structure like create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Body from './Components/Body';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
