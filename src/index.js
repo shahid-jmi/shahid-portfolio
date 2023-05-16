2b596aa2bb7c1e13ac83aa1a5b70a1384f7d57e4
Shahid ul Islam <shahidsalafi4@gmail.com>
2023-05-16 22:41:25 +0530

    Router Configured with react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Portfolio from './Components/Portfolio';
+import About from './Components/About';
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={appRouter} />
 );
 
