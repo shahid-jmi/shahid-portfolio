f2e9bfeddc3599eeb47fcef2d802eb65b9716bb8
Shahid ul Islam <i.shahid@ebisuinc.net>
2023-07-23 12:20:26 +0530

    design changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/images/logo.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo.png';
+
 const Title = () => {
     return (
-        <Link to="/" className='flex'>
-            <img className="h-12" src={logo} alt="Shahid ul islam logo" />
-            <p className='py-3'>SHAHID UL ISLAM</p>
+        <Link to="/" className='flex items-center text-white'>
+            <img className="h-12 mr-2" src={logo} alt="Shahid ul islam logo" />
+            <p className='text-xl font-bold'>SHAHID UL ISLAM</p>
         </Link>
-    )
+    );
 }
 
 const Header = () => {
     return (
-        <div className='bg-black border-red-600 border-solid text-white'>
-            <header className='flex justify-between m-4'>
+        <div className='bg-black text-white'>
+            <header className='flex justify-between items-center py-4 px-8'>
                 <Title />
-                <div className='flex'>
-                    <ul className='flex'>
-                        <Link to="/about"> <li className="">About Me</li> </Link>
-                        <Link to="/services"><li className="">Services </li> </Link>
-                        <Link to="/projects"><li className="">Projects </li> </Link>
-                        <Link to="/skills"><li className="">Skills </li> </Link>
-                        <Link to="/contact"><li className="bg-blue-400 rounded-xl">Contact </li> </Link>
-                    </ul>
-                </div>
+                <ul className='flex items-center'> {/* Add items-center class to center the elements horizontally */}
+                    <li className='mr-4'>
+                        <Link to="/about" className="hover:text-blue-400">About Me</Link>
+                    </li>
+                    <li className='mr-4'>
+                        <Link to="/services" className="hover:text-blue-400">Services</Link>
+                    </li>
+                    <li className='mr-4'>
+                        <Link to="/projects" className="hover:text-blue-400">Projects</Link>
+                    </li>
+                    <li className='mr-4'>
+                        <Link to="/skills" className="hover:text-blue-400">Skills</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">
+                            <button className="bg-blue-400 hover:bg-blue-500 py-2 px-4 rounded-md"> {/* Adjust the padding values */}
+                                Contact
+                            </button>
+                        </Link>
+                    </li>
+                </ul>
             </header>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
