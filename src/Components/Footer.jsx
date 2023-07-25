832f7c1eca9db3e357b20c19b2f0ab3f2c0ee800
Shahid ul Islam <i.shahid@ebisuinc.net>
2023-07-25 18:56:23 +0530

    layout changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-black text-white py-4 text-center">
+        <footer className="py-4 text-center glass">
             <h1 className="text-xl font-bold">COME HANG OUT ON INSTAGRAM</h1>
             <a
                 href='https://www.instagram.com/_i_shahid/'
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/images/logo.png';
 
 const Title = () => {
     return (
-        <Link to="/" className='flex items-center text-white'>
+        <Link to="/" className='header-title'>
             <img className="h-12 mr-2" src={logo} alt="Shahid ul islam logo" />
             <p className='text-xl font-bold'>SHAHID UL ISLAM</p>
         </Link>
@@ -13,20 +13,20 @@ const Title = () => {
 
 const Header = () => {
     return (
-        <div className='bg-black text-white'>
-            <header className='flex flex-col sm:flex-row justify-between items-center py-4 px-8'>
-                <div className="flex items-center mb-4 sm:mb-0"> {/* This div is for the title and logo */}
+        <div>
+            <header className='header-container'>
+                <div className="header-title">
                     <Title />
                 </div>
-                <ul className='flex items-center'> {/* Add items-center class to center the elements horizontally */}
-                    <li className='mr-4'>
-                        <Link to="/about" className="hover:text-blue-400">About Me</Link>
+                <ul className='header-links'>
+                    <li>
+                        <Link to="/about">About Me</Link>
                     </li>
-                    <li className='mr-4'>
-                        <Link to="/projects" className="hover:text-blue-400">Projects</Link>
+                    <li>
+                        <Link to="/projects">Projects</Link>
                     </li>
-                    <li className='mr-4'>
-                        <Link to="/skills" className="hover:text-blue-400">Skills</Link>
+                    <li>
+                        <Link to="/skills">Skills</Link>
                     </li>
                     <li className='mr-4'>
                         <Link to="/contact" className="hover:text-blue-400">Contact</Link>
