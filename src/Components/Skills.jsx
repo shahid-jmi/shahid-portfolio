b09109b08abbd1684c305696502d936fa9d12d72
Shahid ul Islam <i.shahid@ebisuinc.net>
2023-07-25 22:29:35 +0530

    skills page data

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
+import React from 'react';
 
 const Skills = () => {
     return (
-        <div>
-            <h1>Skills</h1>
-        </div>
-    )
-}
+        <div className="max-w-3xl mx-auto p-6 bg-white shadow-md rounded-md">
+            <h1 className="text-3xl font-bold mb-4">Skills</h1>
 
-export default Skills
+            <p className="mb-4">
+                As a Full Stack Web Developer, I possess a diverse skill set that enables me to create comprehensive and polished web and mobile solutions. Here are the key skills that I bring to the table:
+            </p>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Languages:</h5>
+                <ul className="list-disc pl-6">
+                    <li>Python</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>PHP</li>
+                </ul>
+            </div>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Front-End:</h5>
+                <ul className="list-disc pl-6">
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript (including jQuery)</li>
+                    <li>React.js</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Mobile Development:</h5>
+                <ul className="list-disc pl-6">
+                    <li>React Native</li>
+                </ul>
+            </div>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Back-End Frameworks:</h5>
+                <ul className="list-disc pl-6">
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>Sails.js</li>
+                    <li>Nest.js</li>
+                    <li>Laravel</li>
+                </ul>
+            </div>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Desktop Applications:</h5>
+                <ul className="list-disc pl-6">
+                    <li>PyQt</li>
+                </ul>
+            </div>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Why Choose Me</h5>
+                <p>
+                    With expertise in HTML5, CSS3, and Bootstrap, I am well-versed in crafting visually appealing and responsive user interfaces that ensure an optimal user experience across various devices and screen sizes. I combine these front-end skills with the power of React.js to build interactive and dynamic web applications that captivate users and drive engagement.
+                </p>
+                <p>
+                    Additionally, my proficiency in React Native allows me to extend my development capabilities to the mobile world. I can create cross-platform mobile applications that seamlessly integrate with your web offerings, ensuring consistency and brand identity across all platforms.
+                </p>
+                <p>
+                    From building robust back-ends with Node.js and Express.js to implementing sophisticated desktop applications with PyQt, I thrive in delivering solutions that are both powerful and user-friendly.
+                </p>
+            </div>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Continuously Learning</h5>
+                <p>
+                    I embrace a learning-driven approach, constantly staying up-to-date with the latest advancements in web and mobile development. I am passionate about honing my skills and exploring emerging technologies, which translates to cutting-edge solutions for my clients.
+                </p>
+            </div>
+
+            <div className="mb-6">
+                <h5 className="font-bold mb-2">Collaboration and Communication</h5>
+                <p>
+                    Effective collaboration and communication are at the core of my development process. I value the insights and feedback from clients and team members, as they contribute to delivering projects that not only meet expectations but exceed them.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
